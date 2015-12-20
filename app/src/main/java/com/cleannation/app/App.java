f613697ea15d450d82d73c1d61d0946dc8afff6c
Varun <varunlakshmananap@gmail.com>
2015-12-20 15:02:49 +0530

    Made it generic for use!

--- app/src/main/java/com/cleannation/app/App.java
+++ app/src/main/java/com/cleannation/app/App.java
@@ -5,22 +5,13 @@ package com.cleannation.app;
  */
 
 import android.app.Application;
-
 import com.parse.Parse;
 
 public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-
         Parse.enableLocalDatastore(this);
-        Parse.initialize(this, "xODHk0yo4gkH6icHADqg4kCMIEpUgtEnro5hSWsf", "HfhpMzFiOdmE4L1zCsIYdp7leCAv7RduX04fjGhm");
-        // [Optional] Power your app with Local Datastore. For more info, go to
-        // https://parse.com/docs/android/guide#local-datastore
-
-
-        //Parse.initialize(this);
-        //ParseAnalytics.trackAppOpened(getApplicationInfo().getIntent());
+        Parse.initialize(this, "Application ID", "Client ID");
     }
 }
